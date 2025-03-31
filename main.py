7c16b8ec36ec796bbfdf977a64a79626f49293aa
inkiei <ingmarwielandts@gmail.com>
2025-03-31 12:34:44 +0200

    main en html

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+# URL van de API waar de JSON data staat
+API_URL = "https://my-json-server.typicode.com/GITHUB_USERNAME/REPO_NAAM/games"
+
+@app.route('/')
+def index():
+    # Haal de data op van de API
+    response = requests.get(API_URL)
+    games = response.json()  # Zet de JSON-data om naar een Python object
+    
+    return render_template('index.html', games=games)
+
+if __name__ == '__main__':
+    app.run(debug=True)
